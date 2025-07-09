01401bc06db2a61965f59ba49ab41969d8329a47
Andrey22 <andreymail22112006@gmail.com>
2025-07-09 19:51:11 +0300

    feat: Добавить первоначальные тесты для фронтегда

---
+++ frontend/tests/unit/components/entities/news_card/NewsCard.spec.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import NewsCard from '../../../../../src/entities/news_card/ui/NewsCardComponent.vue';
+import { INewsCard } from '../../../../../src/entities/news_card/types/news';
+import { formatDate } from '../../../../../src/shared/lib/workingWithDate/formatDate';
+
+describe('NewsCard.vue', () => {
+    it('Отображаются переданные props', () => {
+        const props: INewsCard = {
+            id: 'djskeu48234829',
+            img: '/images/test.jpg',
+            title: 'Тестовая новость',
+            description: 'Описание тестовой новости',
+            date: new Date(2025, 11, 5),
+        };
+
+        const wrapper = mount(NewsCard, {
+            props,
+            global: {
+                stubs: {
+                    RouterLink: {
+                        template: '<a><slot /></a>',
+                    },
+                },
+            },
+        });
+
+        expect(wrapper.text()).toContain(props.title);
+        expect(wrapper.text()).toContain(props.description);
+        expect(wrapper.text()).toContain(formatDate(props.date));
+
+        expect(wrapper.get('img').attributes('src')).toBe(props.img);
+    });
+});
